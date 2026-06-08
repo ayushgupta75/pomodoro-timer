ac08ed93fcd9b27fab386d11dc5a337a4e4bdf15
Ayush Gupta <guptaayush75@gmail.com>
2026-06-08 01:00:29 -0400

    Initial Commit

---
+++ pomodoro timer/pomodoro_timerApp.swift
@@ -0,0 +1,17 @@
+//
+//  pomodoro_timerApp.swift
+//  pomodoro timer
+//
+//  Created by Ayush Gupta on 6/8/26.
+//
+
+import SwiftUI
+
+@main
+struct pomodoro_timerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
